f80b1bc7d545189ffc2794371871434307f9d481
galenskap <jean.deborah@gmail.com>
2024-04-27 10:31:00 +0200

    Install tailwind & daisyui

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" class="btn btn-primary">Home</RouterLink>
+        <RouterLink to="/about" class="btn btn-secondary">About</RouterLink>
       </nav>
     </div>
   </header>
@@ -36,24 +36,6 @@ nav {
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
