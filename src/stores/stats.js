4f63d94718d526ada9bc0461a1225f9b161df114
galenskap <jean.deborah@gmail.com>
2024-05-01 17:15:40 +0200

    Fix stats generation

--- src/stores/stats.js
+++ src/stores/stats.js
@@ -1,51 +1,38 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
+import { usePlayersStore } from './players'
 
 export const useStatsStore = defineStore('stats', () => {
 
     let stats = useStorage('stats', ref([]))
-
+    const players = usePlayersStore();
 
     async function generate(matches) {
         
         // Flatten the matches array to make it easier to calculate the stats
         matches = matches.reduce((acc, round) => acc.concat(round), [])
 
-        const players = []
-        matches.forEach(match => {
-            if (!players.includes(match.player1)) {
-                players.push(match.player1)
-            }
-            if (!players.includes(match.player2)) {
-                players.push(match.player2)
-            }
-        })
-
-        // Remove the dummy player
-        const byeIndex = players.indexOf('#bye')
-        if (byeIndex !== -1) {
-            players.splice(byeIndex, 1)
-        }
-
-        const tempStats = players.map(player => {
-            const playerMatches = matches.filter((match) => (match.player1 === player || match.player2 === player) && !match.fake)
+        const tempStats = players.list.map(player => {
+            const unorderedPlayerMatches = matches.filter((match) => (match.player1 === player || match.player2 === player) && !match.fake)
+            const playerMatches = [];
             // Putting the player in the first position to make it easier to calculate the stats
-            playerMatches.forEach((match, index) => {
+            unorderedPlayerMatches.forEach((match, index) => {
                 if (match.player2 === player) {
-                    const temp = match.player1
-                    playerMatches[index].player1 = match.player2
-                    playerMatches[index].player2 = temp
-                    const tempScore = match.score1
-                    playerMatches[index].score1 = match.score2
-                    playerMatches[index].score2 = tempScore
+                    const temp = {}
+                    temp.player1 = match.player2
+                    temp.player2 = match.player1
+                    temp.score1 = match.score2
+                    temp.score2 = match.score1
+                    playerMatches.push(temp)
+                } else {
+                    playerMatches.push(match)
                 }
             })
             
-            console.log(playerMatches)
-            const winnedMatches =  playerMatches.filter(match => (match.player1 === player && match.score1 > match.score2))
+            const winnedMatches =  playerMatches.filter(match => (match.score1 > match.score2))
             const wins = winnedMatches.length
-            const losses = playerMatches.filter(match => (match.player1 === player && match.score1 < match.score2)).length
+            const losses = playerMatches.filter(match => (match.score1 < match.score2)).length
             const draws = playerMatches.filter(match => match.score1 === match.score2).length
             const pointsScored = playerMatches.reduce((acc, match) =>  acc + parseInt(match.score1), 0)
             const pointsConceded = playerMatches.reduce((acc, match) => acc + parseInt(match.score2), 0)
@@ -55,7 +42,6 @@ export const useStatsStore = defineStore('stats', () => {
             return { player, wins, losses, draws, pointsScored, pointsConceded, goalDifference, points, totalPositiveGoalDifference }
 
         })
-console.log(tempStats);
         stats.value = tempStats
         
     }
