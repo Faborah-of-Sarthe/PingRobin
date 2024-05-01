7c6c1255d487850fcbc0428fa660e9d3c752b194
galenskap <jean.deborah@gmail.com>
2024-05-01 17:22:30 +0200

    Fix progress bar on chrome browser

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center">
         <span class="badge badge-primary badge-sm py-1">{{ matches.currentRound }}</span>
-        <progress class="progress progress-primary w-full mx-2" :value="matches.progress" max="100"></progress>
+        <progress class="progress progress-primary w-full mx-2 min-w-40" :value="matches.progress" max="100"></progress>
         <span class="badge badge-primary badge-sm py-1">{{ matches.list.length }}</span>
     </div>
 </template>
