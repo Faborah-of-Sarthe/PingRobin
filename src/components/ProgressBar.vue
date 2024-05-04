59df1d2a1fad370e3dbc81e1570d8fbe55469a1d
Fwagra <fabiocerqueiracw@gmail.com>
2024-05-04 15:33:49 +0200

    Step badges

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex items-center">
-        <span class="badge badge-primary badge-sm py-1">{{ matches.currentRound }}</span>
+        <span class="badge badge-primary badge-md py-1 text-white font-bold">{{ matches.currentRound }}</span>
         <progress class="progress progress-primary w-full mx-2 min-w-40" :value="matches.progress" max="100"></progress>
-        <span class="badge badge-primary badge-sm py-1">{{ matches.list.length }}</span>
+        <span class="badge badge-primary badge-md py-1 text-white font-bold">{{ matches.list.length }}</span>
     </div>
 </template>
 
